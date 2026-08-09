a5c1c10a31077520d01d1c9c96c2c34454976543
Renata-Ren78 <renatecertoka@yahoo.com>
2026-08-09 06:55:21 +0300

    IlCarro_HW

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,35 @@
+package pages;
+
+import dto.UserLombok;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
+public class LoginPage extends BasePage {
+    public LoginPage(WebDriver driver) {
+        PageFactory.initElements(new AjaxElementLocatorFactory
+                (driver, 10), this);
+    }
+
+    @FindBy(xpath = "//input[@id='email']")
+    WebElement inputEmail;
+
+    @FindBy(xpath = "//input[@id='password']")
+    WebElement inputPassword;
+
+    @FindBy(xpath = "//button[text()='Y’alla!']")
+    WebElement btnYalla;
+
+
+    public void typeLoginRegistrationForm(UserLombok user) {
+        inputEmail.sendKeys(user.getUsername());
+        inputPassword.sendKeys(user.getPassword());
+    }
+
+    public void clickBtnYalla() {
+        btnYalla.click();
+    }
+
+}
--- src/test/java/ui_tests/RegistrationTests.java
+++ src/test/java/ui_tests/RegistrationTests.java
@@ -1,4 +1,28 @@
 package ui_tests;
 
-public class RegistrationTests {
+import dto.UserLombok;
+import manager.AppManager;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class RegistrationTests extends AppManager {
+
+    @BeforeMethod
+    public void goToLoginPage(){
+        new HomePage(getDriver()).clickBtnLogin();
+    }
+
+
+    @Test
+    public void loginPositiveTest(){
+        UserLombok user = UserLombok.builder()
+                .username("renatae_test_new@gmail.com")
+                .password("ren_CER$123")
+                .build();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginRegistrationForm(user);
+        loginPage.clickBtnYalla();
+    }
 }
